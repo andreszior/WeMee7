c1c601f46ba1625c328316dc84e13156ac8589f0
CarlosBlanco <carlostrubia@gmail.com>
2023-12-08 21:46:51 +0100

    Fragment About añadido

--- app/src/main/java/com/example/weMee7/activities/AboutFragment.java
+++ app/src/main/java/com/example/weMee7/activities/AboutFragment.java
@@ -1,66 +1,45 @@
 package com.example.weMee7.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import androidx.fragment.app.Fragment;
 
 import com.example.wemee7.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AboutFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AboutFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AboutFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AboutFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AboutFragment newInstance(String param1, String param2) {
-        AboutFragment fragment = new AboutFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+        setupImageButton(view, R.id.btCreador1, "https://www.linkedin.com/in/andresmonvi/");
+        setupImageButton(view, R.id.btCreador2, "https://es.linkedin.com/in/carlos-blanco-ferm%C3%ADn-b9ab13229");
+        setupImageButton(view, R.id.btCreador3, "https://www.linkedin.com/in/daniel-liñera-aa6088295");
+        setupImageButton(view, R.id.btgithub, "https://github.com/andreszior/WeMee7");
+
+        return view;
+    }
+
+    private void setupImageButton(View view, int buttonId, String url) {
+        ImageButton button = view.findViewById(buttonId);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openUrl(url);
+            }
+        });
+    }
+
+    private void openUrl(String url) {
+        Uri link = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, link);
+        startActivity(intent);
     }
 }
